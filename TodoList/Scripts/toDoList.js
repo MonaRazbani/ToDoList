c86a79e2d915d54bc8bdd1280bf6237b8f971383
MonaRazbani <mona.razbani1@gmail.com>
2022-07-03 16:03:30 +0430

    add session storage

--- TodoList/Scripts/toDoList.js
+++ TodoList/Scripts/toDoList.js
@@ -9,8 +9,42 @@
 
 }
 
-//** @type {Note[]} */
+
+//false ?? true
+//false || true
+//false && true
+
+//let a = 'a'; 
+
+//a = a ?? false;
+
+//a ??= false;
+
+//method?.()
+
+//note?.name
+
+//array?.[0]
+
+//const result = family?.father?.name?.split() ?? 'nope';
+
+//const ternery =
+//    0 > 1 ? 'one'
+//        : 5 == 5 ? 'two'
+//            : 'three';
+
 let allNotes = JSON.parse(`[{"userId":1,"id":1,"title":"delectus aut autem","completed":false},{"userId":1,"id":2,"title":"quis ut nam facilis et officia qui","completed":false},{"userId":1,"id":3,"title":"fugiat veniam minus","completed":false},{"userId":1,"id":4,"title":"et porro tempora","completed":true},{"userId":1,"id":5,"title":"laboriosam mollitia et enim quasi adipisci quia provident illum","completed":false},{"userId":1,"id":6,"title":"qui ullam ratione quibusdam voluptatem quia omnis","completed":false},{"userId":1,"id":7,"title":"illo expedita consequatur quia in","completed":false},{"userId":1,"id":8,"title":"quo adipisci enim quam ut ab","completed":true},{"userId":1,"id":9,"title":"molestiae perspiciatis ipsa","completed":false},{"userId":1,"id":10,"title":"illo est ratione doloremque quia maiores aut","completed":true},{"userId":1,"id":11,"title":"vero rerum temporibus dolor","completed":true},{"userId":1,"id":12,"title":"ipsa repellendus fugit nisi","completed":true},{"userId":1,"id":13,"title":"et doloremque nulla","completed":false},{"userId":1,"id":14,"title":"repellendus sunt dolores architecto voluptatum","completed":true},{"userId":1,"id":15,"title":"ab voluptatum amet voluptas","completed":true},{"userId":1,"id":16,"title":"accusamus eos facilis sint et aut voluptatem","completed":true},{"userId":1,"id":17,"title":"quo laboriosam deleniti aut qui","completed":true},{"userId":1,"id":18,"title":"dolorum est consequatur ea mollitia in culpa","completed":false},{"userId":1,"id":19,"title":"molestiae ipsa aut voluptatibus pariatur dolor nihil","completed":true},{"userId":1,"id":20,"title":"ullam nobis libero sapiente ad optio sint","completed":true},{"userId":2,"id":21,"title":"suscipit repellat esse quibusdam voluptatem incidunt","completed":false},{"userId":2,"id":22,"title":"distinctio vitae autem nihil ut molestias quo","completed":true},{"userId":2,"id":23,"title":"et itaque necessitatibus maxime molestiae qui quas velit","completed":false},{"userId":2,"id":24,"title":"adipisci non ad dicta qui amet quaerat doloribus ea","completed":false},{"userId":2,"id":25,"title":"voluptas quo tenetur perspiciatis explicabo natus","completed":true}]`);
+
+sessionStorage.setItem('allNoteStrage', sessionStorage.getItem('allNoteStrage') || JSON.stringify(allNotes));
+
+//** @type {Note[]} */
+//let allNoteStrage = !Boolean(allNoteStrage) ? sessionStorage.getItem("allNoteStrage") : allNotes;
+
+//let allNoteStrage = !Boolean(allNoteStrage) ? sessionStorage.getItem("allNoteStrage") : allNotes;
+
+//var ss = sessionStorage.getItem("allNoteStrage");
+//sessionStorage.setItem("allNoteStrage", JSON.stringify(allNotes));
+
 
 ///** @type {HTMLInputElement} */
 //const submit = document.getElementById('submit')
@@ -172,6 +206,7 @@ let allNotes = JSON.parse(`[{"userId":1,"id":1,"title":"delectus aut autem","com
 //allNotes[currentTodoIndex].completed = !allNotes[currentTodoIndex].completed;
 
 
+
 const form = document.querySelector('#inputForm');
 
 function renderInsert() {
@@ -179,7 +214,7 @@ function renderInsert() {
 
     form.innerHTML = '';
     const [insertTemplate, onAfterInsertInit] = insert();
-    form.innerHTML = insertTemplate; 
+    form.innerHTML = insertTemplate;
     onAfterInsertInit();
 }
 
@@ -201,15 +236,24 @@ function insert() {
 
         btnInput.addEventListener('click', () => {
 
-            const title = root.querySelector('#noteInput').innerText;
-            allNotes.push(new Note(1, allNotes.length - 1, title, false));
+            const title = root.querySelector('#noteInput').value;
+            const note = new Note(1, Math.random() * 100, title, false)
+            /** @type{Note[]} */
+            let allNoteStrage = JSON.parse(sessionStorage.getItem("allNoteStrage"));
+
+            allNoteStrage.push(note);
+
+            ////sessionStorage.removeItem("allNoteStrage");
+            sessionStorage.setItem("allNoteStrage", JSON.stringify(allNoteStrage));
+
+
             renderTodos();
 
         });
 
 
     }
-        return [InsertTemplate, onAfterInsertInit];
+    return [InsertTemplate, onAfterInsertInit];
 
 }
 
@@ -217,19 +261,22 @@ function renderTodos() {
 
     notes.innerHTML = '';
 
-    for (const note of allNotes) {
+    /**@type{Note[]} */
+    const allNoteStrage = JSON.parse(sessionStorage.getItem("allNoteStrage"));
+
+    for (const note of allNoteStrage) {
         const [template, onAfterInit] = todoComponent(note);
         notes.innerHTML += template;
     }
 
-    for (const note of allNotes) {
+    for (const note of allNoteStrage) {
         const [template, onAfterInit] = todoComponent(note);
         onAfterInit();
     }
 
 }
 
-renderInsert();
+
 
 // renderTodos();
 
@@ -270,20 +317,33 @@ function todoComponent(note, afterInit = undefined) {
 
         //check or uncheck
         checkbox.addEventListener('click', () => {
+            /** @type{Note[]} */
+            let allNoteStrage = JSON.parse(sessionStorage.getItem("allNoteStrage"));
 
-            const currentTodoIndex = allNotes.findIndex(i => i.id === note.id);
-            allNotes[currentTodoIndex].completed = !allNotes[currentTodoIndex].completed;
+            const currentTodoIndex = allNoteStrage.findIndex(i => i.id === note.id);
+            allNoteStrage[currentTodoIndex].completed = !allNoteStrage[currentTodoIndex].completed;
+
+           // sessionStorage.removeItem("allNoteStrage");
+            sessionStorage.setItem("allNoteStrage", JSON.stringify(allNoteStrage));
+
 
             renderTodos();
 
         });
 
         //delete 
-        btnDelete.addEventListener('click', () => {
+        btnDelete.addEventListener('click', (event) => {
+
+            event.preventDefault();
+            event.stopPropagation();
 
             //const currentTodoIndex = allNotes.findIndex(i => i.id === note.id);
 
-            allNotes = allNotes.filter(q => q.id !== note.id);
+            let allNoteStrage = JSON.parse(sessionStorage.getItem("allNoteStrage"));
+            allNoteStrage = allNoteStrage.filter(q => q.id !== note.id);
+
+           // sessionStorage.removeItem("allNoteStrage");
+            sessionStorage.setItem("allNoteStrage", JSON.stringify(allNoteStrage));
 
             renderTodos();
 
@@ -298,3 +358,5 @@ function todoComponent(note, afterInit = undefined) {
 
 }
 
+renderTodos();
+renderInsert();
